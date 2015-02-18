20d22ce181e922367295f2dbf44b65abe8c46402
FRAGnat <fragnatt@gmail.com>
2015-02-19 01:04:38 +0300

    first homework with backbone

---
+++ public_html/templates/cssmenu2.js
@@ -0,0 +1,8 @@
+<div class="cssmenu" style="text-align:center; margin: 0 auto; margin-top:200px;">
+			<div class="title" style="background-color:gray;">Land of Dragon Knight and Lady</div>
+			<ul>
+			   <li><a href='#'><span>123</span></a></li>
+			   <li><a href='#'><span>321</span></a></li>
+			   <li><a href='#'><span>444</span></a></li>
+			</ul>
+		</div>
