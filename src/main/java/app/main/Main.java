318f72ebf48663def4caa3eea1aca5238023eefe
Andrey Bondar <Alanai1995@gmail.com>
2015-03-05 17:51:35 +0300

    some fixes

--- src/main/java/app/main/Main.java
+++ src/main/java/app/main/Main.java
@@ -2,7 +2,10 @@ package app.main;
 
 import app.servlets.AdminServlet;
 import app.servlets.Router;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 /**
@@ -19,10 +22,24 @@ public class Main {
         AdminServlet adminServlet = new AdminServlet(server);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
+
+
         context.addServlet(new ServletHolder(adminServlet), "/admin/");
         context.addServlet(new ServletHolder(router), "/api/*");
 
-        server.setHandler(context);
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setResourceBase("public_html");
+
+
+
+        //server.setHandler(context);
+
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] {resourceHandler, context});
+
+        server.setHandler(handlers);
 
         server.start();
         server.join();
