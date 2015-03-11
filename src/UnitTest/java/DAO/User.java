3b4340943242334c937a42809ab9f8c9aaedce89
FRAGnat <fragnatt@gmail.com>
2015-03-11 14:49:40 +0300

    add test getUserById

--- src/UnitTest/java/DAO/User.java
+++ src/UnitTest/java/DAO/User.java
@@ -4,6 +4,8 @@ package DAO;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import DAO.logic.UserLogic;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -12,7 +14,23 @@ import java.util.Collection;
 public class User {
 
     @Test
-    public void testEmptyCollection() {
+    public void testGetUserById() {
+        try {
+            UserDAO userModel = Factory.getInstance().getUserDAO();
+            UserLogic user = userModel.getUserById(1);
+            assertNotNull(user);
+            assertTrue(!user.getEmail().isEmpty());
+            assertTrue(!user.getUsername().isEmpty());
+            assertTrue(user.getId() == 1);
+            assertTrue(user.getLevel() > 0);
+            assertTrue(!user.getPassword().isEmpty());
+            //assertTrue(user.getRegistration());
+            user = userModel.getUserById(999999);
+            assertTrue(user == null);
+
+        } catch (Exception e){
+            assertTrue(false);
+        }
         Collection collection = new ArrayList();
         assertNotNull(collection);
         assertTrue(collection.isEmpty());
